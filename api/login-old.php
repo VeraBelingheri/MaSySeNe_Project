8aaf91bc324aab48d70a7f1aa6dee21e29fa2e14
a-crea <alessio.crea98@gmail.com>
2020-12-15 18:02:11 +0100

    name with ps

--- api/login-old.php
+++ api/login-old.php
@@ -8,15 +8,8 @@
 		$password = stripslashes($_POST['password']);
 		$password = mysqli_real_escape_string($con,$password);
 		$password=hash('SHA512',$password);
-
-		$stmt = $con->prepare("SELECT id, name FROM `users` WHERE email= ? and password= ?");
-        $stmt->bind_param("ss", $email, $password); 
-        $con->query("START TRANSACTION");
-		$stmt->execute();
-		$result = $stmt->get_result();
-		$stmt->close();
-		$con->query("COMMIT");
-
+		$query = "SELECT id, name FROM `users` WHERE email='$email' and password='$password'";
+		$result = mysqli_query($con,$query) or die(mysql_error());
 		$rows = mysqli_num_rows($result);
 		if($rows==1){
 			while($row = mysqli_fetch_assoc($result)){
