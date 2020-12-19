f24595b1183c9933a8a71f6c11836818a4da7ec8
a-crea <alessio.crea98@gmail.com>
2020-12-19 23:10:38 +0100

    header origin commented

--- api/db/connect.php
+++ api/db/connect.php
@@ -6,6 +6,7 @@ if (mysqli_connect_errno())
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
   header('Access-Control-Allow-Origin: http://localhost/project-team-insecure/'); 
+  // header('Access-Control-Allow-Origin: http://localhost:3000'); 
   // header('Access-Control-Allow-Headers: Content-Type');
   header('Access-Control-Allow-Headers: Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization');
   header('Access-Control-Allow-Credentials: true'); 
